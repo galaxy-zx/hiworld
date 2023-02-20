08a261b659586bb48c9a4d0cded0cd436320f2b3
galaxy-zx <galaxyzx555@163.com>
2023-02-20 11:27:54 +0800

    新增函数

--- src/main/java/org/example/A.java
+++ src/main/java/org/example/A.java
@@ -1,10 +1,16 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class A {
     public static void main(String[] args) {
         System.out.println("ddd");
         int a=1;
         List<Integer> list=new ArrayList<>();
         int b=1;
+    }
+    public void a(){
+        System.out.println("zx");
     }
 }
