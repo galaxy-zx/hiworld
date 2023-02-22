35dbed85b2d36cb99b587216c32ad14fcb3d4ec0
galaxy-zx <galaxyzx555@163.com>
2023-02-22 17:31:14 +0800

    修改

--- src/main/java/org/example/A.java
+++ src/main/java/org/example/A.java
@@ -11,11 +11,14 @@ public class A {
         int b=1;
     }
 
-    public void a() {
+    public void c() {
 
     }
        public void b() {
-           System.out.println(1);
+           System.out.println(222);
+
+    }
+    public void d() {
 
     }
 
